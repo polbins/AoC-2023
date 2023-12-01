47f981d1e4357d22468efd09b15dd9bed4b752e5
Paul Intal <pintal@squareup.com>
2023-11-30 22:15:37 -0800

    Day1 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach { line ->
+            val first = line.first { it.isDigit() }
+            val last = line.last { it.isDigit() }
+            println("$first$last".toInt())
+            sum += "$first$last".toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +16,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
